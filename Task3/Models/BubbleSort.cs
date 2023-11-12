4fdeb60ccd6f7c2cf7bdefd539b9b0d6e85b1c72
evan_kirk <evankirk@vk.com>
2023-11-13 02:18:13 +0500

    Реализован метод Sort() Bubble-сортировки

--- Task3/Models/BubbleSort.cs
+++ Task3/Models/BubbleSort.cs
@@ -2,9 +2,58 @@
 {
     internal class BubbleSort
     {
-        public void Sort()
+        public BubbleSort(List<string> words)
         {
+            Sort(words);
+        }
 
+        private List<string>? _sortedList;
+        public List<string> SortedList
+        {
+            get
+            {
+                return _sortedList;
+            }
+
+            set
+            {
+                _sortedList = value;
+            }
+        }
+
+        private void Sort(List<string> words)
+        {
+            if (words == null)
+                return;
+
+            bool swapped;
+            do
+            {
+                swapped = false;
+                for (int i = 1; i < words.Count; i++)
+                {
+                    if (string.Compare(words[i - 1], words[i]) > 0)
+                    {
+                        string temp = words[i - 1];
+                        words[i - 1] = words[i];
+                        words[i] = temp;
+                        swapped = true;
+                    }
+                }
+            } while (swapped);
+
+            // Сохраняем порядок одинаковых слов
+            for (int i = 1; i < words.Count; i++)
+            {
+                if (string.Compare(words[i - 1], words[i]) == 0 && string.Compare(words[i - 1], words[i]) > 0)
+                {
+                    string temp = words[i - 1];
+                    words[i - 1] = words[i];
+                    words[i] = temp;
+                }
+            }
+
+            SortedList = words;
         }
     }
 }
