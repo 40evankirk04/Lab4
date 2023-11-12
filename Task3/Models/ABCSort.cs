43a00ced5150ae3913ccc46f3b66dc90dcd8f417
evan_kirk <evankirk@vk.com>
2023-11-13 02:14:51 +0500

    Реализация ABC сортировки вместо Radix

---
+++ Task3/Models/ABCSort.cs
@@ -0,0 +1,54 @@
+﻿namespace Task3.Models
+{
+    internal class ABCSort
+    {
+        public ABCSort(List<string> words)
+        {
+            Sort(words);
+        }
+
+        private List<string> _sortedList;
+        public List<string> SortedList
+        {
+            get
+            {
+                return _sortedList;
+            }
+
+            set
+            {
+                _sortedList = value;
+            }
+        }
+
+        private void Sort(List<string> words)
+        {
+            if (words == null || words.Count <= 1)
+                return;
+
+            int length = words.Count;
+            int gap = length / 2;
+
+            while (gap > 0)
+            {
+                for (int i = gap; i < length; i++)
+                {
+                    string temp = words[i];
+                    int j = i;
+
+                    while (j >= gap && string.Compare(words[j - gap], temp) > 0)
+                    {
+                        words[j] = words[j - gap];
+                        j -= gap;
+                    }
+
+                    words[j] = temp;
+                }
+
+                gap /= 2;
+            }
+
+            _sortedList = words;
+        }
+    }
+}
---
+++ Task3/Views/ABCSortWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Task3.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для ABCSortWindow.xaml
+    /// </summary>
+    public partial class ABCSortWindow : Window
+    {
+        public ABCSortWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
