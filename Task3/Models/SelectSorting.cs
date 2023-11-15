c78b52c4c110310db150cb837fed13cce6122fe6
Иван Киров <evankirk@vk.com>
2023-11-15 12:08:03 +0500

    Замена Bubble сортировки на Select сортировку

---
+++ Task3/Models/SelectSorting.cs
@@ -0,0 +1,47 @@
+﻿namespace Task3.Models
+{
+    internal class SelectSorting
+    {
+        public SelectSorting() { }
+
+        public SelectSorting(List<string> words)
+        {
+            Sort(words);
+        }
+
+        private List<string>? _sortedList;
+        public List<string>? SortedList
+        {
+            get
+            {
+                return _sortedList;
+            }
+
+            set
+            {
+                _sortedList = value;
+            }
+        }
+
+        public void Sort(List<string> words)
+        {
+            for (int i = 0; i < words.Count - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < words.Count; j++)
+                {
+                    if (string.Compare(words[j], words[minIndex], StringComparison.Ordinal) < 0)
+                    {
+                        minIndex = j;
+                    }
+                }
+                if (minIndex != i)
+                {
+                    (words[minIndex], words[i]) = (words[i], words[minIndex]);
+                }
+            }
+
+            _sortedList = words;
+        }
+    }
+}
